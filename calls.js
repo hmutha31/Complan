9f133f3d3d4059d9f245a2e92c7eff0f8860f2c0
Harsh Mutha <harshmutha31@gmail.com>
2017-12-27 17:29:19 +0530

    code change

--- calls.js
+++ calls.js
@@ -3,119 +3,128 @@ var refreshTime=30000;
 
 function getData()
 {
-	console.log("1");
-	$("#tbody").html("");
-	var base_url_string="https://s3.ap-south-1.amazonaws.com/virtualveda/";
-    var base_url = new URL(base_url_string);
-    console.log(base_url);
-	var url_string = window.location.href;
-	var url = new URL(url_string);
-	var c = url.searchParams.get("device");
-    var date= $("#selected_date").val(); //variable for date filter;
-    //conso'.log
-    console.log(date);
-    if(date==undefined || date == ""){
-    	date = "";
-    }
-    else{
-    	date = "/"+date;
-    }
-    console.log(date);
-	var api_url = "http://dev.virtualveda.in/vv/len/h2h/api/public/mis/calls/"+c+date;
-	console.log(api_url);
-	var rec1_string="";
-	var rec2_string="";
-	var img_string="";
-	var i=1;
-	$.getJSON(api_url,function(data)
-      {
-        $.each(data.calls,function(index,element)
-             {
-             	// console.log(element.recording1);
-             	// rec1_string=base_url+element.recording1;
-             	// var rec1 = new URL(rec1_string);
-             	// // console.log(rec1);
-             	// rec2_string=base_url+element.recording2;
-              //   var rec2 = new URL(rec2_string);
-              var rec1;
-              var rec2;
-                if(element.recording1=="")
-                {
-                	rec1_string=base_url+element.recording1;
-                }
-                else
-                {
-                	rec1_string=base_url+element.recording1;
-                	rec1=new URL(rec1_string);
-                }
+    $.ajax(
+    {
+    	type : 'GET',
+    	url : "http://dev.virtualveda.in/vv/len/h2h/api/public/mis/calls/"+c+date,
+    	dataType : 'json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', "Bearer "+token);
+        },
+        success : function(data)
+        {
+					console.log("1");
+					$("#tbody").html("");
+					var base_url_string="https://s3.ap-south-1.amazonaws.com/virtualveda/";
+				    var base_url = new URL(base_url_string);
+				    console.log(base_url);
+					var url_string = window.location.href;
+					var url = new URL(url_string);
+					var c = url.searchParams.get("device");
+				    var date= $("#selected_date").val(); //variable for date filter;
+				    //conso'.log
+				    console.log(date);
+				    if(date==undefined || date == ""){
+				    	date = "";
+				                                     }
+				    else{
+				    	date = "/"+date;
+				        }
+				    console.log(date);
+					// var api_url = "http://dev.virtualveda.in/vv/len/h2h/api/public/mis/calls/"+c+date;
+					// console.log(api_url);
+					var rec1_string="";
+					var rec2_string="";
+					var img_string="";
+					var i=1;
+				        $.each(data.calls,function(index,element)
+				             {
+				             	// console.log(element.recording1);
+				             	// rec1_string=base_url+element.recording1;
+				             	// var rec1 = new URL(rec1_string);
+				             	// // console.log(rec1);
+				             	// rec2_string=base_url+element.recording2;
+				              //   var rec2 = new URL(rec2_string);
+				              var rec1;
+				              var rec2;
+				                if(element.recording1=="")
+				                {
+				                	rec1_string=base_url+element.recording1;
+				                }
+				                else
+				                {
+				                	rec1_string=base_url+element.recording1;
+				                	rec1=new URL(rec1_string);
+				                }
 
-                if(element.recording2=="")
-                {
-                	rec2_string=base_url+element.recording2;
-                }
-                else
-                {
-                	rec2_string=base_url+element.recording2;
-                	rec2=new URL(rec2_string);
-                }
-                // var img = element.photo;
-                if(element.photo=="")
-                {
-                img_string = base_url+element.photo;
-                 // var img_url = new URL(img_string);
-                }
-                else
-                {
-                	img_string=base_url+element.photo;
-                	var img_url=new URL(img_string);
-                }
-                // img_string = base_url+element.photo;
-                // var img_url = new URL(img_string);
-                // console.log(img_url);
-              $("#tbody").append(
-               "<tr>" + 
-               "<td class='no'>" + i+"</td>"+
-               "<td class='promoter'>" + validateObj(element.username) + "</td>" +
-               "<td class='saleType'>" + validateObj(element.sale_type) + "</td>" +
-               "<td class='consumer_mobile'>" + validateObj(element.mobile_number) + "</td>"+
-               "<td class='feedText'>" +   validateFeedback(element.feedback_text) + " </td>"+
-               "<td class='location'>" + "<a target='_blank' href='http://maps.google.com/?q="+ validateObj(element.lat) +"," + validateObj(element.lng)+"'>" +validateObj(element.lat) +"," + validateObj(element.lng)+"</a>" + "</td>"+
-               // "<td class='recording'>"+"1." +"<audio controls preload='none'>"+"<source src='"+validateRec(rec1)+"'>" + "</audio>"+ "<br>" +
-               // "2."+"<audio controls preload='none'>"+"<source src='"+validateRec(rec2)+"'>" + "</audio>" +"</td>"+
-               "<td class='recording'>" + "1." + validateRec(rec1) + "<br>" + "2." + validateRec(rec2) + "</td>" +
-               // "<td class='recording'>" +"<audio controls>"+"<source src='rec1'>" + "</audio>"+
-               "<td class='photo'>" +  validatePhoto(img_url) + "</td>"+
-               //"<td class='photo'>" + "<img class='thumb' width='50' height='50' src='"+img_url+"' data-src='"+img_url+"' >"+"</td>" +
-               "<td class='date_time'>" + element.created + "</td>" +
-               "<td class='choice'>" + validateChoice(element.complan) +"</td>"+
-               "<td class='choice'>" + validateChoice(element.horlicks) +"</td>"+
-               "<td class='choice'>" + validateChoice(element.boost) +"</td>"+
-               "<td class='choice'>" + validateChoice(element.bournvita) +"</td>"+
-               "<td class='choice'>" + validateChoice(element.other) +"</td>"+
-               "<td class='choice'>" + validateChoice(element.none) +"</td>"+
-               // "<td>" +validateChoice(element.none)+"</td>"+
-               "<td class='interactions'>" + validateInteraction(element.new) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.start) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.protein_challenge) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.play_video) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.promo_offer) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.sale) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.sample) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.mobile) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.otp) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.dosage) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.feedback) + "</td>"+
-                "<td class='interactions'>" + validateInteraction(element.end) + "</td>"+
-                // "<td class='interactions'>" + validateInteraction(element.created) + "</td>"+
-                "</tr>"+ i++
-                
-              	)//end of append         
+				                if(element.recording2=="")
+				                {
+				                	rec2_string=base_url+element.recording2;
+				                }
+				                else
+				                {
+				                	rec2_string=base_url+element.recording2;
+				                	rec2=new URL(rec2_string);
+				                }
+				                // var img = element.photo;
+				                if(element.photo=="")
+				                {
+				                img_string = base_url+element.photo;
+				                 // var img_url = new URL(img_string);
+				                }
+				                else
+				                {
+				                	img_string=base_url+element.photo;
+				                	var img_url=new URL(img_string);
+				                }
+				                // img_string = base_url+element.photo;
+				                // var img_url = new URL(img_string);
+				                // console.log(img_url);
+				              $("#tbody").append(
+				               "<tr>" + 
+				               "<td class='no'>" + i+"</td>"+
+				               "<td class='promoter'>" + validateObj(element.username) + "</td>" +
+				               "<td class='saleType'>" + validateObj(element.sale_type) + "</td>" +
+				               "<td class='consumer_mobile'>" + validateObj(element.mobile_number) + "</td>"+
+				               "<td class='feedText'>" +   validateFeedback(element.feedback_text) + " </td>"+
+				               "<td class='location'>" + "<a target='_blank' href='http://maps.google.com/?q="+ validateObj(element.lat) +"," + validateObj(element.lng)+"'>" +validateObj(element.lat) +"," + validateObj(element.lng)+"</a>" + "</td>"+
+				               // "<td class='recording'>"+"1." +"<audio controls preload='none'>"+"<source src='"+validateRec(rec1)+"'>" + "</audio>"+ "<br>" +
+				               // "2."+"<audio controls preload='none'>"+"<source src='"+validateRec(rec2)+"'>" + "</audio>" +"</td>"+
+				               "<td class='recording'>" + "1." + validateRec(rec1) + "<br>" + "2." + validateRec(rec2) + "</td>" +
+				               // "<td class='recording'>" +"<audio controls>"+"<source src='rec1'>" + "</audio>"+
+				               "<td class='photo'>" +  validatePhoto(img_url) + "</td>"+
+				               //"<td class='photo'>" + "<img class='thumb' width='50' height='50' src='"+img_url+"' data-src='"+img_url+"' >"+"</td>" +
+				               "<td class='date_time'>" + element.created + "</td>" +
+				               "<td class='choice'>" + validateChoice(element.complan) +"</td>"+
+				               "<td class='choice'>" + validateChoice(element.horlicks) +"</td>"+
+				               "<td class='choice'>" + validateChoice(element.boost) +"</td>"+
+				               "<td class='choice'>" + validateChoice(element.bournvita) +"</td>"+
+				               "<td class='choice'>" + validateChoice(element.other) +"</td>"+
+				               "<td class='choice'>" + validateChoice(element.none) +"</td>"+
+				               // "<td>" +validateChoice(element.none)+"</td>"+
+				               "<td class='interactions'>" + validateInteraction(element.new) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.start) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.protein_challenge) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.play_video) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.promo_offer) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.sale) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.sample) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.mobile) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.otp) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.dosage) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.feedback) + "</td>"+
+				                "<td class='interactions'>" + validateInteraction(element.end) + "</td>"+
+				                // "<td class='interactions'>" + validateInteraction(element.created) + "</td>"+
+				                "</tr>"+ i++
+				                
+				              	)//end of append         
 
-             }//end of each
-        	)//end of each
-      
-      }//end of json
-		)//end of json
+				             } //end of each
+				        	);//end of each
+        }
+    });
+				      
+				      
 }//end of fn getData
 
 function validateObj(obj)
