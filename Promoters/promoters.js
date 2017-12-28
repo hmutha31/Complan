169d928f68da4a5dbf972774afe7c6b3bde0b320
Harsh Mutha <harshmutha31@gmail.com>
2017-12-28 14:25:24 +0530

    code edit

--- Promoters/promoters.js
+++ Promoters/promoters.js
@@ -1,6 +1,7 @@
 function getData()
 {
 	var token1 = _readCookie('token1');
+	console.log(token1);
 	 $.ajax(
 	 {
        type : 'GET' ,
@@ -19,6 +20,7 @@ function getData()
            "<td class='data'>" + element.id + "</td>" +
            "<td class='data'>" + element.mobile + "</td>" +
            "<td class='data'>" + element.password + "</td>"+
+           "<td class='delete'>delete</td>" + 
            "</tr>"
            )
        })//end of each
@@ -33,9 +35,7 @@ function _readCookie(name) {
     var ca = document.cookie.split(';');
      for(var i=0;i < ca.length;i++) {
         var c = ca[i];
-	console.log("readCookie c:"+c);
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
-	console.log("readCookie c:"+c);
 
         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
